684a32e11a88c8909c21ffc944dcb5f70afd3185
cureos <anders@cureos.com>
2011-12-14 22:23:31 +0100

    Added Rosenbrock Post Office problem to Silverlight test

--- Cureos.Numerics/hs071_sl/MainPage.xaml.cs
+++ Cureos.Numerics/hs071_sl/MainPage.xaml.cs
@@ -20,14 +20,17 @@ namespace hs071_sl
 
         private void StartOptButton_Click(object sender, RoutedEventArgs e)
         {
-            /* create the IpoptProblem */
+            /* create the IpoptProblems */
             var p = new HS071();
+            var r = new RosenbrockPostOffice();
 
             /* allocate space for the initial point and set the values */
             double[] x = { 1.0, 5.0, 5.0, 1.0 };
 
             try
             {
+                var output = new StringBuilder();
+
                 double obj;
                 var problem = Ipopt.CreateIpoptProblem(p._n, p._x_L, p._x_U, p._m, p._g_L, p._g_U, p._nele_jac,
                                                          p._nele_hess, IpoptIndexStyle.C,
@@ -44,10 +47,31 @@ namespace hs071_sl
                 // Free problem resources.
                 Ipopt.FreeIpoptProblem(problem);
 
-                var output = new StringBuilder();
+                output.AppendLine("HS071");
+                output.AppendLine("=====");
                 output.AppendFormat("Optimization return status: {0}{1}{1}", status, Environment.NewLine);
                 output.AppendFormat("Objective function value f = {0}{1}", obj, Environment.NewLine);
                 for (int i = 0; i < 4; ++i) output.AppendFormat("x[{0}]={1}{2}", i, x[i], Environment.NewLine);
+                output.AppendLine();
+
+                // Also optimize Rosenbrock post office problem
+                problem = Ipopt.CreateIpoptProblem(r._n, r._x_L, r._x_U, r._m, r._g_L, r._g_U, r._nele_jac,
+                                                         r._nele_hess, IpoptIndexStyle.C,
+                                                         r.eval_f, r.eval_g, r.eval_grad_f, r.eval_jac_g, r.eval_h);
+                Ipopt.AddIpoptStrOption(problem, "hessian_approximation", "limited-memory");
+                Ipopt.AddIpoptIntOption(problem, "limited_memory_max_history", 5);
+#if INTERMEDIATE
+                Ipopt.SetIntermediateCallback(problem, r.intermediate);
+#endif
+
+                x = new[] { 10.0, 10.0, 10.0 };
+                status = Ipopt.IpoptSolve(problem, x, null, out obj, null, null, null, IntPtr.Zero);
+
+                output.AppendLine("Rosenbrock Post Office");
+                output.AppendLine("======================");
+                output.AppendFormat("Optimization return status: {0}{1}{1}", status, Environment.NewLine);
+                output.AppendFormat("Objective function value f = {0}{1}", obj, Environment.NewLine);
+                for (int i = 0; i < 3; ++i) output.AppendFormat("x[{0}]={1}{2}", i, x[i], Environment.NewLine);
 
                 OptOutputTextBox.Text = output.ToString();
             }
