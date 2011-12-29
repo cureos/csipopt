4cbfefe4ec8901b5717751b2a3fe47861b94d76b
cureos <anders@cureos.com>
2011-12-29 23:08:53 +0100

    Added unit tests for managed callback functions in HS040

---
+++ Cureos.Numerics/Tests.Cureos.Numerics/HS040ManagedTests.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) 2010-2011 Anders Gustafsson, Cureos AB.
+// All rights reserved. Any unauthorised reproduction of this 
+// material will constitute an infringement of copyright.
+
+using Cureos.Numerics.Nlp;
+using Cureos.Numerics.Support;
+using NUnit.Framework;
+
+namespace Cureos.Numerics
+{
+    [TestFixture]
+    public class HS040ManagedTests
+    {
+        #region Unit tests
+
+        [Test]
+        public void SolveProblem_HessianApproximation_ReturnsSucceededStatus()
+        {
+            var instance = new HS040(false, true, false);
+            var x = new[] { 0.8, 0.8, 0.8, 0.8 };
+
+            const IpoptReturnCode expected = IpoptReturnCode.Solve_Succeeded;
+            double obj;
+            var actual = instance.SolveProblem(x, out obj, null, null, null, null);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SolveProblem_HessianApproximation_ReturnsTrueOptimalValue()
+        {
+            var instance = new HS040(false, true, false);
+            var x = new[] { 0.8, 0.8, 0.8, 0.8 };
+
+            const double expected = -0.25;
+            double actual;
+            instance.SolveProblem(x, out actual, null, null, null, null);
+
+            Assert.AreEqual(expected, actual, 1e-7);
+        }
+
+        [Test]
+        public void SolveProblem_HessianApproximation_ConstraintsSatisfied()
+        {
+            var instance = new HS040(false, true, false);
+            var x = new[] { 0.8, 0.8, 0.8, 0.8 };
+
+            var expected = new[] { 1.0, 0.0, 0.0 };
+            double obj;
+            var actual = new double[3];
+            instance.SolveProblem(x, out obj, actual, null, null, null);
+
+            CollectionAssert.AreEqual(expected, actual, new DoubleComparer(1.0e-5));
+        }
+
+        [Test]
+        public void SolveProblem_HessianApproximation_VariablesEqualOptimalValues()
+        {
+            var instance = new HS040(false, true, false);
+            var actual = new[] { 0.8, 0.8, 0.8, 0.8 };
+
+            var expected = new[] { 0.793701, 0.707107, 0.529732, 0.840896 };
+            double obj;
+            instance.SolveProblem(actual, out obj, null, null, null, null);
+
+            CollectionAssert.AreEqual(expected, actual, new DoubleComparer(1.0e-5));
+        }
+
+        [Test]
+        public void SolveProblem_HessianApproximation_NumberOfIterationsSufficientlyLow()
+        {
+            var instance = new HS040(false, true, true);
+            var x = new[] { 0.8, 0.8, 0.8, 0.8 };
+
+            const int expected = 20;
+            double obj;
+            instance.SolveProblem(x, out obj, null, null, null, null);
+            var actual = instance.NumberIterations;
+
+            Assert.Greater(actual, 0);
+            Assert.Less(actual, expected);
+        }
+
+        [Test]
+        public void SolveProblem_ExactHessian_NumberOfIterationsLessThanOrEqualToHessianApprox()
+        {
+            // Get number of iterations for Hessian approximation
+            var approx = new HS040(false, true, true);
+            var x = new[] { 0.8, 0.8, 0.8, 0.8 };
+            double obj;
+            approx.SolveProblem(x, out obj, null, null, null, null);
+            var expected = approx.NumberIterations;
+
+            // Get number of iterations for exact Hessian
+            var exact = new HS040(false, false, true);
+            x = new[] { 0.8, 0.8, 0.8, 0.8 };
+            exact.SolveProblem(x, out obj, null, null, null, null);
+            var actual = exact.NumberIterations;
+
+            Assert.LessOrEqual(actual, expected);
+        }
+
+        #endregion
+    }
+}
--- Cureos.Numerics/Tests.Cureos.Numerics/Nlp/HS040.cs
+++ Cureos.Numerics/Tests.Cureos.Numerics/Nlp/HS040.cs
@@ -178,5 +178,135 @@ namespace Cureos.Numerics.Nlp
         }
         
         #endregion
+
+        #region MANAGED CALLBACK FUNCTIONS
+
+        public override bool eval_f(int n, double[] x, bool new_x, out double obj_value)
+        {
+            obj_value = -x[0] * x[1] * x[2] * x[3];
+            return true;
+        }
+
+        public override bool eval_g(int n, double[] x, bool new_x, int m, double[] g)
+        {
+            g[0] = x[0] * x[0] * x[0] + x[1] * x[1];
+            g[1] = x[0] * x[0] * x[3] - x[2];
+            g[2] = x[3] * x[3] - x[1];
+            return true;
+        }
+
+        public override bool eval_grad_f(int n, double[] x, bool new_x, double[] grad_f)
+        {
+            grad_f[0] = -x[1] * x[2] * x[3];
+            grad_f[1] = -x[0] * x[2] * x[3];
+            grad_f[2] = -x[0] * x[1] * x[3];
+            grad_f[3] = -x[0] * x[1] * x[2];
+            return true;
+        }
+
+        public override bool eval_jac_g(int n, double[] x, bool new_x, int m, int nele_jac,
+            int[] iRow, int[] jCol, double[] values)
+        {
+            if (values == null)
+            {
+                /* set the structure of the jacobian */
+                /* this particular jacobian is dense */
+
+                iRow[0] = 0;
+                jCol[0] = 0;
+                iRow[1] = 0;
+                jCol[1] = 1;
+                iRow[2] = 1;
+                jCol[2] = 0;
+                iRow[3] = 1;
+                jCol[3] = 2;
+                iRow[4] = 1;
+                jCol[4] = 3;
+                iRow[5] = 2;
+                jCol[5] = 1;
+                iRow[6] = 2;
+                jCol[6] = 3;
+            }
+            else
+            {
+                /* return the values of the jacobian of the constraints */
+
+                values[0] = 3.0 * x[0] * x[0];  /* 0,0 */
+                values[1] = 2.0 * x[1];         /* 0,1 */
+
+                values[2] = 2.0 * x[0] * x[3];  /* 1,0 */
+                values[3] = -1.0;               /* 1,2 */
+                values[4] = x[0] * x[0];        /* 1,3 */
+
+                values[5] = -1.0;               /* 2,1 */
+                values[6] = 2.0 * x[3];         /* 2,3 */
+            }
+
+            return true;
+        }
+
+        public override bool eval_h(int n, double[] x, bool new_x, double obj_factor, int m, double[] lambda,
+            bool new_lambda, int nele_hess, int[] iRow, int[] jCol, double[] values)
+        {
+            if (values == null)
+            {
+                /* set the Hessian structure. This is a symmetric matrix, fill the lower left
+                 * triangle only. */
+
+                /* the hessian for this problem is actually dense */
+                int idx = 0; /* nonzero element counter */
+                for (int row = 0; row < 4; row++)
+                {
+                    for (int col = 0; col <= row; col++)
+                    {
+                        iRow[idx] = row;
+                        jCol[idx] = col;
+                        idx++;
+                    }
+                }
+
+            }
+            else
+            {
+                /* fill the objective portion */
+                values[0] = 0.0;                            /* 0,0 */
+
+                values[1] = obj_factor * (-x[2] * x[3]);    /* 1,0 */
+                values[2] = 0.0;                            /* 1,1 */
+
+                values[3] = obj_factor * (-x[1] * x[3]);    /* 2,0 */
+                values[4] = obj_factor * (-x[0] * x[3]);    /* 2,1 */
+                values[5] = 0.0;                            /* 2,2 */
+
+                values[6] = obj_factor * (-x[1] * x[2]);    /* 3,0 */
+                values[7] = obj_factor * (-x[0] * x[2]);    /* 3,1 */
+                values[8] = obj_factor * (-x[0] * x[1]);    /* 3,2 */
+                values[9] = 0.0;                            /* 3,3 */
+
+                /* add the portion for the first constraint */
+                values[0] += lambda[0] * (6.0 * x[0]);                      /* 0,0 */
+
+                values[2] += lambda[0] * 2.0;                               /* 1,1 */
+
+                /* add the portion for the second constraint */
+                values[0] += lambda[1] * (2.0 * x[3]);                      /* 0,0 */
+
+                values[6] += lambda[1] * (2.0 * x[0]);                      /* 3,0 */
+
+                /* add the portion for the third constraint */
+                values[9] += lambda[2] * 2.0;                               /* 3,3 */
+            }
+            return true;
+        }
+
+        public override bool intermediate(
+            IpoptAlgorithmMode alg_mod, int iter_count, double obj_value, double inf_pr, double inf_du,
+            double mu, double d_norm, double regularization_size, double alpha_du, double alpha_pr, int ls_trials)
+        {
+            NumberIterations = iter_count;
+            return true;
+        }
+
+        #endregion
     }
 }
