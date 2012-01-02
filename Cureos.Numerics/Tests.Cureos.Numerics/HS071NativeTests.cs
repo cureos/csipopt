a75b2a02c1e19cc905c615ce4e7c283f7fb37e58
cureos <anders@cureos.com>
2012-01-02 23:15:23 +0100

    Migrating optimization problems to unit test project

---
+++ Cureos.Numerics/Tests.Cureos.Numerics/HS071NativeTests.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) 2012 Anders Gustafsson, Cureos AB.
+// All rights reserved. This software and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+
+using System;
+using System.IO;
+using Cureos.Numerics.Nlp;
+using Cureos.Numerics.Support;
+using NUnit.Framework;
+
+namespace Cureos.Numerics
+{
+    [TestFixture]
+    public class HS071NativeTests
+    {
+        #region Fields
+
+        private HS071 _instance;
+
+        #endregion
+
+        #region SetUp and TearDown
+
+        [SetUp]
+        public void Setup()
+        {
+            _instance = new HS071();
+            _instance.AddOption("tol", 1e-7);
+            _instance.AddOption("mu_strategy", "adaptive");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _instance.Dispose();
+            _instance = null;
+        }
+
+        #endregion
+
+        #region Unit tests
+
+        [Test]
+        public void SolveProblem_StandardOptions_SucceededReturnStatus()
+        {
+            double[] x = { 1.0, 5.0, 5.0, 1.0 };
+            double obj;
+
+            const IpoptReturnCode expected = IpoptReturnCode.Solve_Succeeded;
+            var actual = _instance.SolveProblem(x, out obj, null, null, null, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SolveProblem_StandardOptions_OptimalObjectiveValueReturned()
+        {
+            double[] x = { 1.0, 5.0, 5.0, 1.0 };
+
+            const double expected = 17.0140173;
+            double actual;
+            _instance.SolveProblem(x, out actual, null, null, null, null);
+            Assert.AreEqual(expected, actual, 2.0e-7);
+        }
+
+        [Test]
+        public void SolveProblem_StandardOptions_OptimalVectorReturned()
+        {
+            double[] actual = { 1.0, 5.0, 5.0, 1.0 };
+            double obj;
+            var expected = new[] { 1.0, 4.742994, 3.8211503, 1.3794082 };
+            _instance.SolveProblem(actual, out obj, null, null, null, null);
+            CollectionAssert.AreEqual(expected, actual, new DoubleComparer(1.0e-5));
+        }
+
+        [Test]
+        public void AddOption_NameOfOutputFile_FileGenerated()
+        {
+            const string fileName = "hs071.txt";
+            if (File.Exists(fileName)) File.Delete(fileName);
+            _instance.AddOption("output_file", fileName);
+
+            double[] x = { 1.0, 5.0, 5.0, 1.0 };
+            double obj;
+            _instance.SolveProblem(x, out obj, null, null, null, null);
+
+            const bool expected = true;
+            var actual = File.Exists(fileName);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AddOption_PrintUserOptions_ListedInOutputFile()
+        {
+            const string fileName = "hs071.txt";
+            if (File.Exists(fileName)) File.Delete(fileName);
+            _instance.AddOption("output_file", fileName);
+            _instance.AddOption("print_user_options", "yes");
+
+            double[] x = { 1.0, 5.0, 5.0, 1.0 };
+            double obj;
+            _instance.SolveProblem(x, out obj, null, null, null, null);
+            _instance.Dispose();
+
+            const bool expected = true;
+            var actual = File.ReadAllText(fileName).Contains("List of user-set options");
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
---
+++ Cureos.Numerics/Tests.Cureos.Numerics/Nlp/S225.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (C) 2010-2011 Anders Gustafsson and others. All Rights Reserved.
+// This code is published under the Eclipse Public License.
+//
+// Author:  Anders Gustafsson, Cureos AB 2010-12-20
+
+namespace Cureos.Numerics.Nlp
+{
+    /// <summary>
+    /// C# implementation of Schittkowski problem no. 225
+    /// Adapted from http://www.orfe.princeton.edu/~rvdb/ampl/nlmodels/s/s225.mod
+    /// </summary>
+    public class S225 : IpoptProblem
+    {
+        public S225()
+            : base(
+            2,  // Number of variables
+            new[] { NegativeInfinity, NegativeInfinity },   // Lower variable bounds
+            new[] { PositiveInfinity, PositiveInfinity },   // Upper varable bounds
+            5,  // Number of constraints
+            new[] { 1.0, 1.0, 9.0, 0.0, 0.0 },  // Lower constraint bounds
+            new[] { PositiveInfinity, PositiveInfinity, PositiveInfinity, PositiveInfinity, PositiveInfinity }, // Upper constraint bounds
+            10, // Number of elements in (sparse) Jacobian
+            0,  // Number of elements in (sparse) Hessian; 0 since Hessian approximation is applied
+            false,  // Use native callback functions?
+            true,   // Use limited-memory Hessian approximation?
+            false)  // Use intermediate callback function?
+        {
+        }
+
+        public override bool eval_f(int n, double[] x, bool new_x, out double obj_value)
+        {
+            obj_value = x[0] * x[0] + x[1] * x[1];
+
+            return true;
+        }
+
+        public override bool eval_grad_f(int n, double[] x, bool new_x, double[] grad_f)
+        {
+            grad_f[0] = 2.0 * x[0];
+            grad_f[1] = 2.0 * x[1];
+
+            return true;
+        }
+
+        public override bool eval_g(int n, double[] x, bool new_x, int m, double[] g)
+        {
+            g[0] = x[0] + x[1];
+            g[1] = x[0] * x[0] + x[1] * x[1];
+            g[2] = 9.0 * x[0] * x[0] + x[1] * x[1];
+            g[3] = x[0] * x[0] - x[1];
+            g[4] = x[1] * x[1] - x[0];
+
+            return true;
+        }
+
+        public override bool eval_jac_g(int n, double[] x, bool new_x, int m, int nele_jac, int[] iRow, int[] jCol, double[] values)
+        {
+            if (values == null)
+            {
+                /* set the structure of the jacobian */
+                /* this particular jacobian is dense */
+
+                iRow[0] = 0;
+                jCol[0] = 0;
+                iRow[1] = 0;
+                jCol[1] = 1;
+                iRow[2] = 1;
+                jCol[2] = 0;
+                iRow[3] = 1;
+                jCol[3] = 1;
+                iRow[4] = 2;
+                jCol[4] = 0;
+                iRow[5] = 2;
+                jCol[5] = 1;
+                iRow[6] = 3;
+                jCol[6] = 0;
+                iRow[7] = 3;
+                jCol[7] = 1;
+                iRow[8] = 4;
+                jCol[8] = 0;
+                iRow[9] = 4;
+                jCol[9] = 1;
+            }
+            else
+            {
+                /* return the values of the jacobian of the constraints */
+
+                values[0] = 1.0;
+                values[1] = 1.0;
+                values[2] = 2.0 * x[0];
+                values[3] = 2.0 * x[1];
+                values[4] = 18.0 * x[0];
+                values[5] = 2.0 * x[1];
+                values[6] = 2.0 * x[0];
+                values[7] = -1.0;
+                values[8] = -1.0;
+                values[9] = 2.0 * x[0];
+            }
+
+            return true;
+        }
+    }
+}
